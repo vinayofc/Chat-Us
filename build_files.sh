6c2e3e9a0c2eeb77936e5e11e9173e73fe74baf1
Vinay Chaudhary <devilvinayofficial@gmail.com>
2023-06-09 11:09:31 +0530

    first commit

---
+++ build_files.sh
@@ -0,0 +1,11 @@
+
+echo "Building project..."
+python -m pip install -r requirements.txt
+
+echo "Make migrations"
+python manage.py makemigrations
+python manage.py migrate
+
+echo "collect static"
+python manage.py collectstatic --noinput --clear
+
